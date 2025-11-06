811335e66beb3988aca1b8c989cbaf3abfed34b0
shaan-hossain-01 <rockshaan200@gmail.com>
2025-11-06 12:04:14 +0600

    started working on the home layout

---
+++ src/app/(home)/layout.tsx
@@ -0,0 +1,14 @@
+interface Props {
+  children: React.ReactNode;
+}
+
+
+const Layout = ({ children }: Props) => {
+  return (
+    <div>
+      {children}
+    </div >
+  )
+}
+
+export default Layout
