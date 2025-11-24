848ac15793f7f9c69e53031fbac295341482d4fd
shaan-hossain-01 <rockshaan200@gmail.com>
2025-11-24 17:16:37 +0600

    Center triangle pointer on category dropdown button

--- src/app/(app)/(home)/search-filter/CategoryDropdown.tsx
+++ src/app/(app)/(home)/search-filter/CategoryDropdown.tsx
@@ -38,11 +38,12 @@ const CategoryDropdown = ({ category, isActive, isNavigationHovered }: CategoryD
       <div className="relative">
     <Button variant="elevated" className={cn("h-11 px-4 bg-transparent border-transparent hover:bg-white hover:border-primary text-black rounded-full",
       isActive && !isNavigationHovered && "bg-white border-primary",
+      isOpen && "bg-white border-primary shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] -translate-x-[4px] -translate-y-[4px]"
     )}>
         {category.name}
     </Button>
     <div 
-      className={cn("opacity-0 absolute -bottom-3 w-0 h-0 border-l-10 border-l-transparent border-r-10 border-r-transparent border-b-10 border-b-black -translate-x-1/2", isOpen && "opacity-100",)}
+      className={cn("opacity-0 absolute -bottom-3 left-1/2 w-0 h-0 border-l-10 border-l-transparent border-r-10 border-r-transparent border-b-10 border-b-black -translate-x-1/2", isOpen && "opacity-100",)}
     />
       </div>
       <SubcategoryMenu
